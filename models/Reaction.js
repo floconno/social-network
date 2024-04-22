f137a716f11729aa1feeb7ac257ec1c8e56d45b0
floconno <floconno@asu.edu>
2024-04-22 14:45:15 -0700

    created reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+});
+
+const Reaction = schema('Reaction', reactionSchema);
+
+module.exports = Reaction;
